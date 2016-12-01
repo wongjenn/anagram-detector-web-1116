bfe88e3497bf9bbb246164ed50ff484db4df88ba
Jennifer Wong <wongjenn@yahoo.com>
2016-12-01 02:09:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    sorted_word = @word.split("").sort.join("")
+    word_array.select do |w|
+       w.split("").sort.join("") == sorted_word
+    end
+  end
+end
